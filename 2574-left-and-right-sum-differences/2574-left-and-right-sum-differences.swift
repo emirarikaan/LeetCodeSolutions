528bdc51576250c0a918d3927af8757824540102
Emir Arıkan <87249316+emirarikaan@users.noreply.github.com>
2023-07-05 23:04:44 +0300

    Time: 49 ms (64.21%), Space: 14.3 MB (72.63%) - LeetHub

---
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.swift
@@ -0,0 +1,20 @@
+class Solution {
+    func leftRightDifference(_ nums: [Int]) -> [Int] {
+        var sum = 0
+        var result: [Int] = []
+        result.reserveCapacity(nums.count)
+
+        for i in 0..<nums.count {
+            result.append(sum)
+            sum += nums[i]
+        }
+
+        sum = 0
+        for i in 0..<nums.count {
+            result[nums.count-1-i] = abs(result[nums.count-1-i] - sum)
+            sum += nums[nums.count-1-i]
+        }
+
+        return result
+    }
+}
